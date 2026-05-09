5ca1e7a614dc6d2af7e4780cf121287366498e45
Agustin Bustamante <agusbustam@estudiante.uc.cl>
2026-05-08 20:15:49 -0400

    placa ?

--- Personal/ESP_32/Intento_reconocimiento_placa/src/main.cpp
+++ Personal/ESP_32/Intento_reconocimiento_placa/src/main.cpp
@@ -1,12 +1,60 @@
 #include <Arduino.h>
 
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
-  Serial.print("Chip Model: ");
-  Serial.println(ESP.getChipModel());
-  Serial.print("Chip Revision: ");
-  Serial.println(ESP.getChipRevision());
+// Definir pin visual
+const int pinLED = 2;
+// variables metronomo
+int bpm = 60;
+int intervaloDeGolpe = 1000;
+
+TaskHandle_t TareaVisual;
+
+// --- Tarea Visual ---
+void metronomoVisual(void * parameter) {
+  for(;;) {
+    // Parpadeo LED
+    digitalWrite(pinLED, HIGH);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+    digitalWrite(pinLED, LOW);
+
+    // Esperar golpe siguiente
+    vTaskDelay((intervaloDeGolpe - 50) / portTICK_PERIOD_MS);
+  }
 }
 
-void loop() {}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(pinLED, OUTPUT);
+
+  Serial.println("Metronomo Iniciado");
+  Serial.println("Puedes escribir un numero para cambiar los BPM");
+
+  // Asignar tarea visual al nucleo 0
+  xTaskCreatePinnedToCore(
+    metronomoVisual,
+    "Tarea_LED",
+    1000,
+    NULL,
+    1,
+    &TareaVisual,
+    0);
+}
+
+void loop() {
+  // Revisar Input
+  if (Serial.available() > 0) {
+    int nuevoBPM = Serial.parseInt();
+
+    // Validar que sea un ritmo rasonable
+    if (nuevoBPM >= 30 && nuevoBPM <= 200) {
+      bpm = nuevoBPM;
+      intervaloDeGolpe = 60000 / bpm;
+
+      Serial.print(">>> Ritmo Ajustado ");
+      Serial.print(bpm);
+      Serial.println(" BPM");
+    }
+  }
+  // put your main code here, to run repeatedly:
+  vTaskDelay(100 / portTICK_PERIOD_MS); // this speeds up the simulation
+}
