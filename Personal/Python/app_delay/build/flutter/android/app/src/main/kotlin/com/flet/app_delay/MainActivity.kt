0deb1d8d45e9b6fb42f30d7b124bf84339c8feb8
Agustin Bustamante <AgusBustam@estudiante.uc.cl>
2026-05-18 19:30:59 -0400

    Commit de panico

---
+++ Personal/Python/app_delay/build/flutter/android/app/src/main/kotlin/com/flet/app_delay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flet.app_delay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Personal/Python/app_delay/main.py
@@ -0,0 +1,85 @@
+import flet as ft
+import requests
+
+def main(page: ft.Page):
+
+    page.title = "Control de Pedal ESP32"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.vertical_alignment = ft.CrossAxisAlignment.CENTER
+    page.window_wisth = 400
+    page.window_height = 700
+
+    texto_conexion = ft.Text("Esperando la conexión...", size=16, color=ft.colors.RED)
+
+    valor_estado = ft.Text("-", size=18, weight=ft.FontWeight.BOLD, color=ft.color.BLUE)
+    valor_bateria = ft.Text("-", size=18, weight=ft.FontWeight.BOLD)
+    valor_sd = ft.Text("-", size=18, weight=ft.FontWeight.BOLD)
+
+    panel_info = ft.card(
+        elevation=5,
+        content=ft.Container(
+            padding=20,
+            width=320,
+            content=ft.Column(
+                [
+                    ft.row([ft.Icon(ft.icons.SPEED), ft.Text("Telemetria en Vivo", size=20, weight=ft.FontWeight.BOLD)], alignment=ft.MainAxisAlignment.CENTER),
+                    ft.DIvider(),
+                    ft.Row([ft.Text("Modulo Actual:"), valor_estado], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+                    ft.Row([ft.Text("Nivel de Batería:"), valor_bateria], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+                    ft.Row([ft.Text("Memoria SD:"), valor_sd], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
+                ]
+            )
+        )
+    )
+
+    def actualizar_datos(e):
+        texto_conexion.value = "Consultando al ESP32..."
+        texto_conexion.color = ft.colors.ORANGE
+        page.update()
+        try:
+
+            esp32_url = "http://192.168.4.1/api/estado"
+            respuesta = requests.get(esp32_url, timeout=3)
+
+            if respuesta.status_code == 200:
+
+                datos_json = respuesta.json()
+
+                valor_estado.value = str(datos_json.get("estado", "Error")).capitalize()
+                valor_bateria.value = f"{datos_json.get("bateria", 0)} %"
+                valor_sd.value = str(datos_json.get("tarjeta_sd", "Error")).capitalize()
+
+                texto_conexion.value = "¡Sincronizado con éxito!"
+                texto_conexion.color = ft.colors.GREATERTHAN
+            
+            else:
+                texto_conexion.value = f"Error del servidor: {respuesta.status_code}"
+                texto_conexion.color = ft.colors.RED
+        
+        except Exception as error:
+            texto_conexion.value = "No se logro encontrar la red del pedal."
+            texto_conexion.color = ft.colors.RED
+
+        page.update()
+
+    boton_actualizar = ft.ElevatedButton(
+        text="Sincronizar Pedal",
+        icon=ft.icons.SYNC,
+        on_click=actualizar_datos,
+        scale=1.2,
+        bgcolor=ft.colors.BLUE_700,
+        color=ft.colors.WHITE
+    )
+
+    page.add(
+        ft.Icon(ft.icons.SPEAKER_GROUP, size=70, color=ft.colors.BLUE_GREY_800),
+        ft.Text("Sistema Híbrido V1", size=26, weight=ft.FontWeight.W_900),
+        ft.Divider(height=20, color=ft.colors.TRANSPARENT),
+        panel_info,
+        ft.Divider(height=20, color=ft.colors.TRANSPARENT),
+        boton_actualizar,
+        ft.Divider(height=20, color=ft.colors.TRANSPARENT),
+        texto_conexion
+    )
+
+ft.app(target=main)
